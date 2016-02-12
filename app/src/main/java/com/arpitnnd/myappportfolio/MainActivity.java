8cce889c09ad00fffc9524f3e9868c5b7fce9546
Arpit Anand <arpit_nnd@outlook.com>
2016-02-13 04:53:01 +0530

    * Fix hardcoded colors and dimensions. * Shrink button OnClick action code.

--- app/src/main/java/com/arpitnnd/myappportfolio/MainActivity.java
+++ app/src/main/java/com/arpitnnd/myappportfolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.arpitnnd.myappportfolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,28 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonOnClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                Toast.makeText(MainActivity.this, "This will launch Spotify Streamer!", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button2:
-                Toast.makeText(MainActivity.this, "This will launch Super Duo!", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button3:
-                Toast.makeText(MainActivity.this, "This will launch Build It Bigger!", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button4:
-                Toast.makeText(MainActivity.this, "This will launch XYZ Reader!", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.button5:
-                Toast.makeText(MainActivity.this, "This will launch My Capstone Project!", Toast.LENGTH_SHORT).show();
-                break;
-        }
-
+        Toast.makeText(MainActivity.this, "This will launch " + ((Button) view).getText() + "!", Toast.LENGTH_SHORT).show();
     }
 
 }
